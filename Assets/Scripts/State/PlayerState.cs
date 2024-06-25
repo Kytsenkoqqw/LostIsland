3a11e9b341b7c72b24f12489c2b1aaed9de956cb
Kytsenkoqqw <136856664+Kytsenkoqqw@users.noreply.github.com>
2024-06-25 17:29:07 +0300

    Add Player State class

---
+++ Assets/Scripts/State/PlayerState.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace State
+{
+    public abstract class PlayerState : MonoBehaviour
+    {
+        public abstract void Hungry();
+
+        public abstract void Temperature();
+
+        public abstract void Sleepy();
+    }
+}
